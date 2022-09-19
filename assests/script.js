26aaf95b16eb225ec971e3615306910de0e89be3
Joy Halliday <tjoyhalliday@gmail.com>
2022-09-19 15:25:17 -0700

    starter updates

---
+++ assests/script.js
@@ -0,0 +1,12 @@
+const quizStart = document.querySelector("#start-quiz");
+const time = document.querySelector("#time");
+const questionOne = document.querySelector("#question-1");
+const questionTwo = document.querySelector("#question-2");
+const questionThree = document.querySelector("#question-3");
+const questionFour = document.querySelector("#question-4");
+
+
+quizStart.setAttribute("click", function() {
+    if //start quiz
+    //want start button to disappear
+})
