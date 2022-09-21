8921b28fad726e043c58edc8e64133dfe695597a
Joy Halliday <tjoyhalliday@gmail.com>
2022-09-20 19:00:48 -0700

    timer added

--- assests/script.js
+++ assests/script.js
@@ -2,14 +2,24 @@ const quizStart = document.querySelector("#start-quiz");
 const intro = document.querySelector("#intro");
 const time = document.querySelector("#time");
 const questions = document.querySelector("#questions");
-const questionsArray = [{title:"Question 1?", choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"], rightAnswer: 1 }, {title:"Question 2", choices: ["Answer A", "Answer B", "Answer C", "Answer D"], rightAnswer: 1 }, {title:"Question 1?", choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"], rightAnswer: 1 }, {title:"Question 1?", choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"], rightAnswer: 1 }];
+const questionsArray = [{title:"Question 1?", choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"], rightAnswer: 1 }, {title:"Question 2", choices: ["Answer A", "Answer B", "Answer C", "Answer D"], rightAnswer: 1 }, {title:"Question 3?", choices: ["Answer 5", "Answer 6", "Answer 7", "Answer 8"], rightAnswer: 1 }, {title:"Question 4?", choices: ["Answer E", "Answer F", "Answer G", "Answer H"], rightAnswer: 1 }];
 const currentQuestion = document.querySelector("#currentQuestion");
 const nextQuestion = document.querySelector("#next");
 const question = document.querySelector("#question");
-// const questionOne = document.querySelector("#question-1");
-// const questionTwo = document.querySelector("#question-2");
-// const questionThree = document.querySelector("#question-3");
-// const questionFour = document.querySelector("#question-4");
+
+
+let secondsLeft = 45
+
+function startTimer() {
+    const timerInterval= setInterval(function() {
+        secondsLeft--;
+        time.textContent = secondsLeft + "Seconds Left";
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+        }
+    }, 1000);
+}
 
 let currentIndex = 0;
 
@@ -18,12 +28,13 @@ quizStart.addEventListener("click", function(){
     intro.style.display = "none";
     questions.style.display = "block";
     appendNewQuestion();
+    
 });
 
 nextQuestion.addEventListener("click", function(){
-    //question.remove();
     currentIndex = currentIndex + 1;
     appendNewQuestion();
+    console.log("I am a timer");
 });
 
 function appendNewQuestion(){
@@ -36,6 +47,9 @@ function appendNewQuestion(){
         list.appendChild(node);
         question.appendChild(list);
     });
+    if (questionsArray[currentIndex < 5]);
+        console.log("Oops")
+
 }
 
 
