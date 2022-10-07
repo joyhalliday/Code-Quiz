c03b46767fe0d8f0a766363ae04d6a829c777d33
Joy Halliday <tjoyhalliday@gmail.com>
2022-10-07 16:25:33 -0700

    changed buttons functionality for quiz answers

--- assests/script.js
+++ assests/script.js
@@ -8,25 +8,26 @@ const questionsArray = [
 
         choices: ["A Bat", "A Tiger", "A Flying Squirel", "An Eagle"], 
 
-        rightAnswer: 1 }, 
+        rightAnswer: "A Bat" }, 
 
     {title:"How many bones does a giraffe have in its neck?", 
 
         choices: ["45", "12", "7", "9"], 
 
-        rightAnswer: 3 }, 
+        rightAnswer: "7" }, 
 
     {title:"What percentage of DNA do humans share with chimpanzees?", 
 
         choices: ["52.7%", "98.8%", "0%", "95.5%"], 
 
-        rightAnswer: 2 }, 
+        rightAnswer: "98.8%" }, 
 
     {title:"How many dogs are in the world?", 
     
         choices: ["900 million", "4", "675 million", "470 million"], 
 
-        rightAnswer: 4 }];
+        rightAnswer: "470 million" },
+    ];
     
 const currentQuestion = document.querySelector("#currentQuestion");
 const nextQuestion = document.querySelector("#next");
@@ -37,8 +38,7 @@ const submitScore = document.querySelector("#submit");
 let secondsLeft = 25;
 
 
-function startTimer() {
-   
+function startTimer() {  
     const timerInterval= setInterval(function() {
         secondsLeft--;
         time.textContent = secondsLeft + " Seconds Left";
@@ -69,28 +69,62 @@ quizStart.addEventListener("click", function()
 
 
 
-nextQuestion.addEventListener("click", function(){
-    currentIndex = currentIndex + 1;
-    appendNewQuestion();
-});
+// nextQuestion.addEventListener("click", function(){
+//     currentIndex = currentIndex + 1;
+//     if (currentIndex < questionsArray.length) {
+//         appendNewQuestion();
+//     } else {
+//         quizEnd();
+//     }
+    
+// });
+function choiceClick(event) {
+    var buttonEl = event.target;
+    if (!buttonEl.matches(".choice")) {
+        return;
+    }
+    if (buttonEl.value !== questionsArray[currentIndex].rightAnswer) {
+        secondsLeft-=5;
+        if (secondsLeft < 0) {
+            secondsLeft = 0;   
+        } 
+        time.textContent = secondsLeft + " Seconds Left";
+    } 
+    currentIndex++;
+    if (secondsLeft <= 0 || currentIndex === questionsArray.length) {
+        quizEnd();
+    } else {
+        appendNewQuestion();
+    }
+}
+
+answer.onClick = choiceClick;
 
 function appendNewQuestion() {
     const currentQuestionObj = questionsArray[currentIndex];
     answer.innerHTML = ""
     currentQuestion.innerHTML = currentQuestionObj.title;
-    currentQuestionObj.choices.forEach(elem => {
-        const list = document.createElement("Li");
-        const node = document.createTextNode(elem);
-        list.appendChild(node);
-        answer.appendChild(list);
-    });
+    for (var i=0; i<currentQuestionObj.choices.length; i++) {
+        var choice = currentQuestionObj.choices[i];
+        var choiceNode = document.createElement("button");
+        choiceNode.setAttribute("class", "choice");
+        choiceNode.setAttribute("value", choice);
+        choiceNode.textContent = i+1+". "+choice;
+        answer.appendChild(choiceNode);
+    } 
+    // currentQuestionObj.choices.forEach(elem => {
+    //     const list = document.createElement("button");
+    //     const node = document.createTextNode(elem);
+    //     list.appendChild(node);
+    //     answer.appendChild(list);
+    // });
 
-   
- 
-    if (currentQuestionObj.title === ("How many dogs are in the world?")) {
-        quizEnd();
-        console.log(secondsLeft);
-    }    
+    
+
+    // if (currentQuestionObj.title === ("end of quiz")) {
+    //     quizEnd();
+    //     console.log(secondsLeft);
+    // }    
 
 }
 
